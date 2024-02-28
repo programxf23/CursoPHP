d8deebb0a564724b913deddc0d9ff856b4552ba4
Fernando <fh1273509@gmail.com>
2024-02-28 17:44:15 -0300

    comentarios

---
+++ comentarios.php
@@ -0,0 +1,14 @@
+<?php
+
+
+//comentarios de una sola linea
+
+# tambien podemos utilizar la almohadilla
+echo "hola fer febrero 2024";
+
+/*comentarios de multiples lienas */
+
+
+echo "todo comentarios";
+
+?>
