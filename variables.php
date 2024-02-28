913d3d9893917404965df3a731aed6fea5264786
Fernando <fh1273509@gmail.com>
2024-02-28 18:14:10 -0300

    designacion de variables

---
+++ variables.php
@@ -0,0 +1,17 @@
+<?php
+/*las variables en php comienzan con un signo $ seguido del nombre de la  variable
+tiene que empezar con una letra o un guion bajo seguido de cualquier numero de letras
+, numeros, caracteres
+no puede empezar con un numero o simbolos
+el nombre de las variables es sensible a las mayusculas, minusculas, se puede usar el camelcase
+*/
+
+$nombre = "Fernando";
+echo $nombre;
+
+$edad = 35;
+
+echo $edad;
+
+
+
