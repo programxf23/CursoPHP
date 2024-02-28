20ee857d2e6229ba2567ccb0e40c7fd9a11a063a
Fernando <fh1273509@gmail.com>
2024-02-28 17:56:48 -0300

    tipo de datos

---
+++ tipo_datos.php
@@ -0,0 +1,21 @@
+<?php
+
+/*tipo de datos:
+
+boleanos: true(1), false(0)
+enteros: (integer) 10,7,-5,9
+flotantes, (doubles): 5.6, 6.8, 7.3
+cadenas, (strings): "cadenas de texto"
+*/
+
+echo true;
+echo false;
+echo 10;
+
+//nos muestra el tipo de dato:
+var_dump (-10);
+
+var_dump(7.9);
+
+var_dump("hola mundo");
+
