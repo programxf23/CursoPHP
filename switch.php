b7d991e02f8ee27e72e28752a5b9cf07b73ec294
Fernando <fh1273509@gmail.com>
2024-03-17 13:37:28 -0300

    switch

---
+++ switch.php
@@ -0,0 +1,17 @@
+<?php
+
+$fruta = "fresa";
+
+switch($fruta){
+    case "fresa":
+        echo "la fruta es fresa";
+        break;
+    case "manzana": 
+        echo "la fruta es manzana";
+        break;
+    case "pera":
+        echo "la fruta es pera";
+        break;
+    default:
+
+}
