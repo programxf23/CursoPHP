a4d75970bc4cf5889be0a4a28da2ff543bf30ec7
Fernando <fh1273509@gmail.com>
2024-03-29 10:48:00 -0300

    mayusculas y minusculas convertir

---
+++ funcionesFormatear.php
@@ -0,0 +1,12 @@
+<?php
+
+$cadena_texto = "HOLA FER!";
+
+echo strtolower($cadena_texto);
+
+//
+
+$cadena_texto2 = "hola a todos en el mundo";
+$cadena_texto2 = strtoupper($cadena_texto2);
+
+echo $cadena_texto2;
