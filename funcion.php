3229b92719e3016ed3cfdf849e946a3a39f69efa
Fernando <fh1273509@gmail.com>
2024-03-29 10:40:56 -0300

    llamar a una funcion desde otro archivo

---
+++ funcion.php
@@ -0,0 +1,6 @@
+<?php
+function promedio($nota1, $nota2, $nota3){
+
+    $promedio_alumno = ($nota1 + $nota2 + $nota3) / 3;
+    return $promedio_alumno;
+     }
---
+++ llamarFuncion.php
@@ -0,0 +1,14 @@
+<?php
+/*
+
+include "funcion.php";
+echo "el promedio de la funcion es: ". promedio(3,5,9);
+*/
+
+
+// tambien funciona con require:
+
+require "funcion.php";
+echo "el promedio de la funcion es: ". promedio(8,5,9);
+
+
