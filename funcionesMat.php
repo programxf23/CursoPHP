89790c38b9038316f4e931a30d28707b43210319
Fernando <fh1273509@gmail.com>
2024-04-02 13:56:00 -0300

    algunas funciones matematicas

---
+++ funcionesMat.php
@@ -0,0 +1,25 @@
+<?php
+//exponente
+//(5,3) => base, y exponente
+
+echo pow(5, 3);
+
+echo "\n";
+//raiz
+
+echo sqrt(25);
+
+echo "<br/>";
+//numeros aleatorios
+echo rand(1, 100);
+
+//pi
+echo pi();
+
+//redondear numeros a enteros
+echo floor(4.3); //quita los decimales
+echo "\n";
+echo ceil(4.3); //redondear numeros hacia arriba
+
+echo "<br/>";
+echo round(4.3); 
